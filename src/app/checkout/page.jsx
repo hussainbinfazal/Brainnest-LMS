fa38f9dc63c3b7aba15ba1fcd49e8ea097b29c58
hussainbinfazal <mdhussain0610@gmail.com>
2025-06-15 03:44:32 +0530

    change in metadata

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -3,7 +3,7 @@
 
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import useAuthRedirect from '@/hooks/useAuthRedirect';
 export default function Checkout({ course, user }) {
   const [loading, setLoading] = useState(false);
