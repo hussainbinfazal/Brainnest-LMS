61c995ca46fc811740014b73629a860032107042
hussainbinfazal <mdhussain0610@gmail.com>
2026-04-29 03:12:05 +0530

    sign route added in the cloudinary

--- src/app/api/upload/sign/route.ts
+++ src/app/api/upload/sign/route.ts
@@ -17,7 +17,7 @@ export async function POST(request: CustomNextRequest): Promise<NextResponse> {
             logger.warn(`Invalid file type upload attempt by user ${authUser.id} from IP: ${request.ip}`);
             return NextResponse.json({ error: "Invalid file type. Only 'image' and 'video' are allowed." }, { status: 400 });
         }
-        
+
         let timestamp: number = Math.floor(new Date().getTime() / 1000);
         let paramsToSign;
         if (type === "image") {
@@ -25,14 +25,14 @@ export async function POST(request: CustomNextRequest): Promise<NextResponse> {
                 timestamp,
                 folder: process.env.CLOUDINARY_UPLOAD_FOLDER,
                 resource_type: "image",
-                allowed_formats: ["jpg", "jpeg", "png", "webp", "gif"],
+                allowed_formats: "jpg, jpeg, png, webp, gif",
             }
         } else if (type === "video") {
             paramsToSign = {
                 timestamp,
                 folder: process.env.CLOUDINARY_UPLOAD_FOLDER,
                 resource_type: "video",
-                allowed_formats: ["mp4", "mov", "avi", "mkv", "webm"]
+                allowed_formats: "mp4, mov, avi, mkv, webm"
             }
         } else {
             return NextResponse.json({
@@ -40,21 +40,22 @@ export async function POST(request: CustomNextRequest): Promise<NextResponse> {
             }, { status: 400 })
         }
 
-    const api_Secret: string = process.env.CLOUDINARY_API_SECRET!
-    const signature = cloudinary.utils.api_sign_request(paramsToSign, api_Secret);
-    logger.info("Generated signature for user:", { name: authUser.name })
-    return NextResponse.json({
-        signature,
-        timestamp,
-        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-        apiKey: process.env.CLOUDINARY_API_KEY,
-        folder: paramsToSign.folder
+        const api_Secret: string = process.env.CLOUDINARY_API_SECRET!
+        const signature = cloudinary.utils.api_sign_request(paramsToSign, api_Secret);
+        logger.info("Generated signature for user:", { name: authUser.id, type, ip: request.ip });
+        return NextResponse.json({
+            signature,
+            timestamp,
+            cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+            apiKey: process.env.CLOUDINARY_API_KEY,
+            folder: paramsToSign.folder,
+            resourceType: paramsToSign.resource_type,
 
-    }, { status: 200 })
+        }, { status: 200 })
 
-} catch (error: any) {
-    const message = error instanceof Error ? error.message : "An unknown error occurred";
-    logger.error("Signature generation failed:", { error: message });
-    return NextResponse.json({ error: message }, { status: 500 })
-}
+    } catch (error: any) {
+        const message = error instanceof Error ? error.message : "An unknown error occurred";
+        logger.error("Signature generation failed:", { error: message });
+        return NextResponse.json({ error: message }, { status: 500 })
+    }
 }
